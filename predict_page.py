698fbaf5e7764ca9e64b8ed32fd138f22af4016f
chun7144 <63179860+chun7144@users.noreply.github.com>
2023-10-28 09:24:31 +0900

    Add files via upload

---
+++ predict_page.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+
+def load_model():
+    with open('saved_model.pkl', 'rb') as file:
+        ScoreML_loaded = pickle.load(file)
+    return ScoreML_loaded
+
+ScoreML_loaded = load_model()
+
+def show_predict_page():
+    from Score_App import X_mean
+    from Score_App import X_std
+    st.title("Score Prediction")
+
+    st.write("""### We need some information to predict the Score """)
+
+    time_to_study = st.number_input('공부시간 입력', 1, 100)
+
+    ok = st.button("Calculate Score")
+
+    if ok:
+        value = ScoreML_loaded.predict(np.array([[(time_to_study-X_mean)/X_std]]))
+        st.write('점수', value)
